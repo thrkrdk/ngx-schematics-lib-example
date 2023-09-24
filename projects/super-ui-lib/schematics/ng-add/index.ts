a9fa91229e17911f8113aabae184e77292a1dce5
thrkrdk <thrkardak@gmail.com>
2023-09-24 19:11:50 +0300

    modified app module

--- projects/super-ui-lib/schematics/ng-add/index.ts
+++ projects/super-ui-lib/schematics/ng-add/index.ts
@@ -1,34 +1,73 @@
-import { Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import {
-    NodeDependency,
-    NodeDependencyType,
-    getPackageJsonDependency,
-    addPackageJsonDependency,
-    removePackageJsonDependency,
-  } from '@schematics/angular/utility/dependencies';
+  Rule,
+  Tree,
+  SchematicContext,
+  SchematicsException,
+  UpdateRecorder,
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+import {
+  NodeDependency,
+  NodeDependencyType,
+  getPackageJsonDependency,
+  addPackageJsonDependency,
+  removePackageJsonDependency,
+} from '@schematics/angular/utility/dependencies';
+
+import * as ts from 'typescript';
 
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext): Tree => {
-    context.logger.info('Installing Dependencies')
+    context.logger.info('Installing Dependencies');
     const dep: NodeDependency = {
-        type: NodeDependencyType.Dev,
-        name: 'moment',
-        version: '~2.27.0',
-        overwrite: true,
-      };
-  
-      addPackageJsonDependency(tree, dep);
-      console.log(getPackageJsonDependency(tree, 'moment'))
-      // { type: 'devDependencies', name: 'moment', version: '~2.27.0' }
-  
-      removePackageJsonDependency(tree, 'protractor');
-      console.log(getPackageJsonDependency(tree, 'protractor'))
-      // null
+      type: NodeDependencyType.Dev,
+      name: 'moment',
+      version: '~2.27.0',
+      overwrite: true,
+    };
+
+    addPackageJsonDependency(tree, dep);
+    console.log(getPackageJsonDependency(tree, 'moment'));
+    // { type: 'devDependencies', name: 'moment', version: '~2.27.0' }
+
+    removePackageJsonDependency(tree, 'protractor');
+    console.log(getPackageJsonDependency(tree, 'protractor'));
+    
+
+    //modifying app module
+
+    const modulePath = '/src/app/app.module.ts';
+    if (!tree.exists(modulePath)) {
+      throw new SchematicsException(`The file ${modulePath} is not found`);
+    }
+
+    const recorder: UpdateRecorder = tree.beginUpdate(modulePath); 
+    const text = tree.read(modulePath); 
+    if (text === null) {
+      throw new SchematicsException(`The file ${modulePath} is empty`);
+    }
   
 
-      context.addTask(new NodePackageInstallTask(), []);
+    context.logger.info('dfddd3');
+    const source = ts.createSourceFile(
+      modulePath,
+      text.toString(),
+      ts.ScriptTarget.ESNext,
+      true
+    );
 
-    return tree
+    context.logger.info('dfddd4');
+    applyToUpdateRecorder(
+      recorder,
+      addImportToModule(source, modulePath, 'SuperUiLibModule', 'super-ui-lib')
+    );
+
+    tree.commitUpdate(recorder);
+
+    context.addTask(new NodePackageInstallTask(), []);
+
+    return tree;
   };
 }
